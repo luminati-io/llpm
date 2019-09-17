8b01844f640ea65a77cc3f7950ee764d7e6baf20
krzysztof <sysadmin@hola.org>
2019-09-17 11:13:06 +0300

    added license

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+#!/usr/bin/env node
+// LICENSE_CODE ZON ISC
+'use strict'; /*jslint node:true, esnext:true*/
 const yargs = require('yargs');
-
 const args = process.argv.slice(2).map(String);
 const argv = yargs(args).argv;
 
